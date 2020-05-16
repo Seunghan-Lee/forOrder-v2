ea7673c15d20307d39a46249cfbf21d43d3a7353
porcoRosso <jushan1980@gmail.com>
2020-05-17 01:13:58 +0900

    셋팅

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'src/header.php';
+?>
+
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root">Hello</div>
+  </body>
+</html>
